5941bc77b9809ee0eacfce9b47a7a7a521c4c9d7
kunihei <57933493+kunihei@users.noreply.github.com>
2021-05-01 13:46:17 +0900

    ボタンのタグの判別機能を実装

--- PomodoroApp/Controller/ViewController.swift
+++ PomodoroApp/Controller/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var setTimer = Int()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func settimer(_ sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            setTimer = 15
+        case 2:
+            setTimer = 30
+        case 3:
+            setTimer = 60
+        default:
+            setTimer = 90
+        }
+        performSegue(withIdentifier: "timer", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "timer"{
+            let timerVC = segue.destination as! TimerViewController
+        }
+    }
+    
 }
 
